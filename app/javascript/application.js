48519bf7ab943a0c26cb2a534d17ae03ec208c46
iceland101113 <iceland101113@gmail.com>
2023-02-05 00:03:31 +0800

    add jquery to implement open and close comment form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import './src/jquery'
+
+$(function() {
+  $( ".comment-form-display" ).click(function(){
+    $(this).next().child().css("display", "block");
+  });
+
+  $( ".comment-form-close" ).click(function(){
+    $(this).parent().css("display", "none");
+  });
+})
