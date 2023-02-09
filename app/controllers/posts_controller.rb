dbb6a0c2c0b50c1dfec502ad05c96133a0fdb2d9
iceland101113 <iceland101113@gmail.com>
2023-02-09 16:35:26 +0800

    optimize comment component: only login could see reply

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
-  before_action :set_post, :only => [:show, :edit, :update, :destroy, :upvote]
+  before_action :set_post, :except => [:index, :new, :create]
 
   def index
     @posts = Post.includes(:user)
